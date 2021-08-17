c77548075ea535cc20890ef8df838d17f716d01b
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-17 08:26:54 +0545

    fetched json files using async method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header";
+import "./css/App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [subscriptions, setSubscriptions] = useState([]);
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get("users.json");
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getSubscriptions = async () => {
+    try {
+      const {data} = await axios.get("subscriptions.json");
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const usersData = getUsers();
+    const subscriptionsData = getSubscriptions();
+    console.log(usersData);
+    console.log(subscriptionsData);
+  }, []);
   return (
     <div className="App">
-      <h1> Hello</h1>
-   </div>
+      <Header />
+    </div>
   );
 }
 

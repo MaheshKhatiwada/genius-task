a504379ce2390b979b72c78281299ed05833761f
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-19 09:53:09 +0545

    added sorting functionalities

--- src/components/Subscribers.js
+++ src/components/Subscribers.js
@@ -1,44 +1,72 @@
-import React,{useState,useEffect} from "react";
+import React, { useState } from "react";
 import { getSubscribers } from "../common/getSubscribers";
-import "../css/Subscribers.css"
+import "../css/Subscribers.css";
 
 const Subscribers = ({ subscriptions, users }) => {
+  const [search, setSearch] = useState("");
+  const [sorting, setSorting] = useState("");
 
-  const [search,setSearch]=useState('');
-  const [filteredData,setFilteredData]=useState([]);
-
-  useEffect(() => {
-
-    setFilteredData()
-  }, [])
+  const handleSort = (sort) => {
+    setSorting(sort);
+  };
   const displayData = () => {
     const allUsers = getSubscribers(users, subscriptions);
     const subscribers = allUsers.filter(
       (user) => user.userSubscription.length > 0
     );
-    const filteredSubscribers=subscribers.filter(subscriber=> subscriber.first_name.toLowerCase().includes(search.toLocaleLowerCase()))
-    const subscriberUsers = filteredSubscribers.length>0 ?filteredSubscribers.map((subscriber) => {
-      const { id,first_name, middle_name, last_name, username ,join_date,country} = subscriber;
-      return (
-        <div className="subscribers__data" key={id}>
-          <h3>Name : {`${first_name}  ${middle_name} ${last_name} `}</h3>{" "}
-          <h3> Username: {username}</h3>
-          <h3>Country: {country}</h3>
-          <h3>Join Date : {join_date}</h3>
-        </div>
-      );
-    }):"No Match";
+    const filteredSubscribers = subscribers.filter((subscriber) =>
+      subscriber.first_name.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+    const sorted = filteredSubscribers.sort((a, b) => {
+      if (sorting === "asc") {
+        return a.first_name > b.first_name ? 1 : -1;
+      } else if (sorting === "desc") {
+        return a.first_name < b.first_name ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+    const subscriberUsers =
+      sorted.length > 0
+        ? sorted.map((subscriber) => {
+            const {
+              id,
+              first_name,
+              middle_name,
+              last_name,
+              username,
+              join_date,
+              country,
+            } = subscriber;
+            return (
+              <div className="subscribers__data" key={id}>
+                <h3>Name : {`${first_name}  ${middle_name} ${last_name} `}</h3>{" "}
+                <h3> Username: {username}</h3>
+                <h3>Country: {country}</h3>
+                <h3>Join Date : {join_date}</h3>
+              </div>
+            );
+          })
+        : "No Match";
     return subscriberUsers;
   };
 
   return (
-      <>
+    <>
       <h1 className="heading">Subscribers</h1>
-      <div className="inputSearch">
-      <input  type="text" placeholder="Search by First Name" onChange={(e)=>setSearch(e.target.value)}/>
+      <div>
+        <button onClick={() => handleSort("asc")}>Asc Sort</button>
+        <button onClick={() => handleSort("desc")}>Desc Sort</button>
       </div>
-  <div className="subscribers">{displayData()}</div>
-  </>
+      <div className="inputSearch">
+        <input
+          type="text"
+          placeholder="Search by First Name"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="subscribers">{displayData()}</div>
+    </>
   );
 };
 
